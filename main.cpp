19531d5f032c8feec2e1a178c0d80965d6302bd6
A01706219 <A01706219@itesm.mx>
2020-05-12 17:14:30 -0500

    version 2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 using namespace std;
 
 int main(){
-  cout<<"Quina es ggggA2Q4WEFAVSDZXVCFBGVHBJKHNOKÍ0Ñ9K8UOT58645E34WEAWR<WSXVBNM,.Ñ.{ggggei"<<endl;
+  cout<<"Quina es ggggA2Q4WEFAVSDZXVCsdgdfgsdfgsdfgsdjsafhdfglkasdsfhdfgkljashdfkljashdflkjFBGVHBJKHNOKÍ0Ñ9K8UOT58645E34WEAWR<WSXVBNM,.Ñ.{ggggei"<<endl;
   return 0;
 }
